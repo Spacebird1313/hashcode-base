d249a5f27a737b14a12daa6bf4cd06c884db39c6
SirBlackser <dries.blontrock@student.uantwerpen.be>
2019-02-28 19:15:34 +0100

    add sort algoritm

---
+++ src/main/java/be/stivizu/projects/hashcode/algorithm/AlgorithmDries.java
@@ -0,0 +1,9 @@
+package be.stivizu.projects.hashcode.algorithm;
+
+public class AlgorithmDries extends Algorithm {
+
+    @Override
+    protected void doAlgorithm() {
+
+    }
+}
--- src/main/java/be/stivizu/projects/hashcode/model/InputData.java
+++ src/main/java/be/stivizu/projects/hashcode/model/InputData.java
@@ -1,5 +1,7 @@
 package be.stivizu.projects.hashcode.model;
 
+import be.stivizu.projects.hashcode.util.SortPhotosUtil;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +13,10 @@ public class InputData {
      */
 
     protected List<Photo> photos;
+    public SortPhotosUtil sortPhotosUtil;
 
     public InputData(final List<String> fileData) {
+        sortPhotosUtil = new SortPhotosUtil();
         photos = new ArrayList<>();
         int numberOfPhotos = Integer.parseInt(fileData.get(0));
         for (int i = 1; i <= numberOfPhotos; i++) {
@@ -24,6 +28,7 @@ public class InputData {
             }
             photos.add(photo);
         }
+        sortPhotosUtil.sortPhotos(photos);
     }
 
 }
