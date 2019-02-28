09d1e50ab8025f8fd7dd2597ce3f536d6ab35a33
Thomas Huybrechts <huybthomas@gmail.com>
2019-02-28 22:19:45 +0100

    Update performance interest factor

--- src/main/java/be/stivizu/projects/hashcode/algorithm/HorizontalAlgorithm.java
+++ src/main/java/be/stivizu/projects/hashcode/algorithm/HorizontalAlgorithm.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import static be.stivizu.projects.hashcode.util.ThomasUtil.getInterestFactor;
+
 public class HorizontalAlgorithm extends Algorithm
 {
     @Override
@@ -140,58 +142,6 @@ public class HorizontalAlgorithm extends Algorithm
 
         outputData.setNumberOfSlides(slides.size());
         outputData.setSlides(slides);
-    }
-
-    public int getInterestFactor(Photo photo1, Photo photo2)
-    {
-        Set<String> tags1 = photo1.getTags();
-        Set<String> tags2 = photo2.getTags();
-
-        int union = 0;
-        int photo1Ex = tags1.size();
-        int photo2Ex = tags2.size();
-
-        for(String tagThis : tags1)
-        {
-            for(String tagOther : tags2)
-            {
-                if(tagThis.equals(tagOther))
-                {
-                    union++;
-                    photo1Ex--;
-                    photo2Ex--;
-
-                    break;
-                }
-            }
-        }
-
-        return Math.min(Math.min(photo1Ex, photo2Ex), union);
-    }
-
-    public int getInterestFactor(Set<String> tags1, Set<String> tags2)
-    {
-
-        int union = 0;
-        int photo1Ex = tags1.size();
-        int photo2Ex = tags2.size();
-
-        for(String tagThis : tags1)
-        {
-            for(String tagOther : tags2)
-            {
-                if(tagThis.equals(tagOther))
-                {
-                    union++;
-                    photo1Ex--;
-                    photo2Ex--;
-
-                    break;
-                }
-            }
-        }
-
-        return Math.min(Math.min(photo1Ex, photo2Ex), union);
     }
 
 }
