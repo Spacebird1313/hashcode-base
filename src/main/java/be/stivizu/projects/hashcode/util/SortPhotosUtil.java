0279be0e724bd90489bd2ebbda99a37319062b56
SirBlackser <dries.blontrock@student.uantwerpen.be>
2019-02-28 20:18:42 +0100

    add combo algo

--- src/main/java/be/stivizu/projects/hashcode/util/SortPhotosUtil.java
+++ src/main/java/be/stivizu/projects/hashcode/util/SortPhotosUtil.java
@@ -3,11 +3,12 @@ package be.stivizu.projects.hashcode.util;
 import be.stivizu.projects.hashcode.model.Orientation;
 import be.stivizu.projects.hashcode.model.Photo;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class SortPhotosUtil {
-    protected Set<Photo> photosV;
-    protected Set<Photo> photosH;
+    protected Set<Photo> photosV = new HashSet<>();
+    protected Set<Photo> photosH = new HashSet<>();
 
     public Set<Photo> getPhotosV() {
         return photosV;
---
+++ src/main/java/be/stivizu/projects/hashcode/util/VerticalPhotoCombinerUtil.java
@@ -0,0 +1,48 @@
+package be.stivizu.projects.hashcode.util;
+
+import be.stivizu.projects.hashcode.model.Photo;
+import be.stivizu.projects.hashcode.model.Slide;
+
+import java.util.*;
+
+public class VerticalPhotoCombinerUtil {
+
+    public Set<Slide> combineBig(Set<Photo> photos) {
+        Set<Slide> slides = new HashSet<>();
+        List<Photo> photos1 = new ArrayList<>(photos);
+        ListIterator listIterator = photos1.listIterator();
+        System.out.println("begining size: " + photos1.size());
+        while(listIterator.hasNext()) {
+            int amountOfTags = 0;
+            List<Integer> comboId = new ArrayList<>();
+            List<Photo> comboPhoto = new ArrayList<>();
+            Photo winner = null;
+            Photo photo = (Photo) listIterator.next();
+            comboId.add(photo.getId());
+            comboPhoto.add(photo);
+            listIterator.remove();
+            System.out.println("size after first in loop: " + photos1.size());
+            while (listIterator.hasNext()) {
+                Photo photo1 = (Photo) listIterator.next();
+                Set<String> union = new HashSet<>(photo.getTags());
+                union.addAll(photo1.getTags());
+                if(union.size() > amountOfTags) {
+                    winner = photo1;
+                    amountOfTags = union.size();
+                    System.out.println("tag origin: " + photo.getTags().size());
+                    System.out.println("union: " + union.size());
+                    System.out.println("tags winner: " + winner.getTags().size());
+                }
+            }
+            photos1.remove(winner);
+            comboId.add(winner.getId());
+            comboPhoto.add(winner);
+            Slide slide = new Slide(comboPhoto, comboId.get(0), comboId.get(1));
+            slides.add(slide);
+            System.out.println("size after: " + photos1.size());
+            winner = null;
+            listIterator = photos1.listIterator();
+        }
+        return slides;
+    }
+}
