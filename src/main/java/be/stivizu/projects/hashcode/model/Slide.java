680e98ac8bc9bda589f18da3d8d658b669d88eb4
Arthur Janssens <ajanssens@nalys-group.com>
2019-02-28 19:53:11 +0100

    tt

--- src/main/java/be/stivizu/projects/hashcode/model/Slide.java
+++ src/main/java/be/stivizu/projects/hashcode/model/Slide.java
@@ -1,11 +1,14 @@
 package be.stivizu.projects.hashcode.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Slide {
 
     List<Integer> photoIds;
+    List<Photo> photos;
 
     public Slide(int... ids) {
         this.photoIds = new ArrayList<>();
@@ -14,4 +17,24 @@ public class Slide {
         }
     }
 
+    public Slide(List<Photo> photos, int... ids) {
+        this.photos = photos;
+        this.photoIds = new ArrayList<>();
+        for (Integer integer : ids) {
+            photoIds.add(integer);
+        }
+    }
+
+    public int getInterestFactor(Slide other){
+//        getAllTags()
+        return 0;
+    }
+
+    public Set<String> getAllTags(){
+        Set<String> tags = new HashSet<>();
+        for (Photo photo : photos) {
+            tags.addAll(photo.getTags());
+        }
+        return tags;
+    }
 }
