fc98c13810174dbc82ba04112e1cf815ee2777a6
Arthur Janssens <ajanssens@nalys-group.com>
2019-02-28 19:24:57 +0100

    t

---
+++ src/main/java/be/stivizu/projects/hashcode/util/ArthurUtil.java
@@ -0,0 +1,20 @@
+package be.stivizu.projects.hashcode.util;
+
+import be.stivizu.projects.hashcode.model.Orientation;
+import be.stivizu.projects.hashcode.model.Photo;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ArthurUtil {
+
+    public static Set<Photo> getWithOrientation(List<Photo> photos, Orientation o){
+        return photos.parallelStream().filter(p -> p.getOrientation().equals(o)).collect(Collectors.toSet());
+    }
+
+    public static Optional<Photo> getFirstWithOrientation(List<Photo> photos, Orientation o){
+        return photos.stream().filter(p -> p.getOrientation().equals(o)).findFirst();
+    }
+}
